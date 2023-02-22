e11fe3384d9becc6ee0639eb28cca3b981111804
Ahmad Alobaid <ahmad88me@gmail.com>
2023-02-22 05:33:04 +0300

    coverage

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,6 @@
 python3 setup.py build_ext --inplace
 python3 setup.py install
 python3 -m unittest discover
+pip3 install coverage
+coverage run -m unittest discover
+bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
