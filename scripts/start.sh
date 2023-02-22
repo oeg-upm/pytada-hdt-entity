d66d32ffb2042aade58ba2fbe99c35e5a3e21b4a
Ahmad Alobaid <ahmad88me@gmail.com>
2023-02-22 05:47:28 +0300

    update codecov

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,6 @@ python3 setup.py install
 python3 -m unittest discover
 pip3 install coverage
 coverage run --source=. --omit=.venv/* -m unittest discover
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+curl -s https://codecov.io/bash > codecovpush.sh
+chmod +x codecovpush.sh
+./codecovpush.sh -t $CODECOV_TOKEN
